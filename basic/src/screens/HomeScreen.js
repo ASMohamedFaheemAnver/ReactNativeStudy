f310786530a0149b9d7e1bf4b96cdc5ba54ad560
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2021-03-28 10:28:05 +0530

    Navigation,

--- basic/src/screens/HomeScreen.js
+++ basic/src/screens/HomeScreen.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { Text, StyleSheet, View } from "react-native";
+import { Text, StyleSheet, View, Button } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
-const HomeScreen = () => {
+const HomeScreen = ({ navigation }) => {
   return (
     <View>
-      <Text style={styles.text}>HomeScreen</Text>
+      <Button
+        onPress={(e) => {
+          navigation.navigate("Components");
+        }}
+        title="Go to components"
+      ></Button>
+      <TouchableOpacity
+        style={styles.button}
+        onPress={(e) => {
+          navigation.navigate("ListScreen");
+        }}
+      >
+        <Text>Go to list screen</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -12,6 +26,12 @@ const HomeScreen = () => {
 const styles = StyleSheet.create({
   text: {
     fontSize: 30,
+  },
+  button: {
+    padding: 10,
+    alignItems: "center",
+    backgroundColor: "pink",
+    marginTop: 20,
   },
 });
 
