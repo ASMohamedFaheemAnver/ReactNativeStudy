3adefb0e1932dd14d3362845058652f7a2c051f1
FreeDoM <jstrfaheem065@gmail.com>
2021-11-07 01:37:03 +0530

    Push notification without forground setup,

--- rnpush/App.js
+++ rnpush/App.js
@@ -1,7 +1,38 @@
-import React from 'react';
+import messaging from '@react-native-firebase/messaging';
+import React, {useEffect} from 'react';
 import {SafeAreaView, ScrollView, StatusBar} from 'react-native';
 
 const App = () => {
+  useEffect(() => {
+    let unsubscribeOnMessage = () => {};
+    let unsubscribeonNotificationOpenedApp = () => {};
+    (async () => {
+      const authStatus = await messaging().requestPermission();
+      const enabled =
+        authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+        authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+      if (enabled) {
+        messaging()
+          .getInitialNotification()
+          .then(async initialRemoteMessage => {
+            if (initialRemoteMessage) console.log({initialRemoteMessage});
+          });
+        unsubscribeOnMessage = messaging().onMessage(async remoteMessage => {
+          console.log({remoteMessage});
+        });
+        // UDEV Need to resolve
+        unsubscribeonNotificationOpenedApp =
+          messaging().onNotificationOpenedApp(async openedRemoteMessage => {
+            console.log({openedRemoteMessage});
+          });
+      }
+    })();
+    return () => {
+      unsubscribeOnMessage();
+      unsubscribeonNotificationOpenedApp();
+      console.log({msg: 'App.onDestroy'});
+    };
+  }, []);
   return (
     <SafeAreaView>
       <StatusBar />
