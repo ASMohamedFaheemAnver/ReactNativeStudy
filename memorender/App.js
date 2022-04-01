335db97c73609f8a1ea6b160bfb349c18aa6e4ca
FreeDoM <jstrfaheem065@gmail.com>
2022-04-01 09:53:13 +0530

    Rerender fix with some strategy,

--- memorender/App.js
+++ memorender/App.js
@@ -1,6 +1,58 @@
-import React from 'react';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {Button, Text, View} from 'react-native';
 const App = () => {
-  return <></>;
+  const [count, setCount] = useState(0);
+  const [property, setProperty] = useState(0);
+  // Need to prevent recreation
+  // const [data, setData] = useState({isEvent: false});
+  // const data = React.useMemo(()=>({isEvent: false}), []);
+  const data = {isEvent: false};
+  useEffect(() => {
+    if (count % 5 === 0 && count != 0) {
+      setProperty(Math.random());
+    }
+  }, [count]);
+  const onPress = useCallback(() => setCount(Math.random()), []);
+  return (
+    <View>
+      <Text>{count}</Text>
+      <Button
+        title="Count ++"
+        onPress={() => {
+          setCount(count + 1);
+        }}></Button>
+      <MemoChild onPress={onPress} property={property} data={data}></MemoChild>
+    </View>
+  );
 };
 
 export default App;
+
+const MemoChild = React.memo(
+  ({onPress, property, data}) => {
+    const renders = useRef(0);
+    const [count, setCount] = useState(0);
+
+    return (
+      <View>
+        <Text>{`Parent property : ${property}`}</Text>
+        <Text>{`Count : ${count}`}</Text>
+        <Text>{`Renders : ${renders.current++}`}</Text>
+        <Button
+          title="Increment"
+          onPress={() => {
+            setCount(count + 1);
+            if (count % 2 === 0) {
+              onPress();
+            }
+          }}></Button>
+      </View>
+    );
+  },
+  (previousProps, nextProps) => {
+    if (previousProps.data.isEvent !== nextProps.data.isEvent) {
+      return false;
+    }
+    return true;
+  },
+);
