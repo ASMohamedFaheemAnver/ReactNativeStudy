ed014faa7edc960e62758e084ee0b0e11f4d5e34
FreeDoM <jstrfaheem065@gmail.com>
2022-04-18 15:03:18 +0530

    F*king hell dood!

--- share/App.js
+++ share/App.js
@@ -11,30 +11,48 @@ const App = () => {
       <ViewShot ref={captureRef}>
         <Button
           onPress={async () => {
-            const uri = await captureRef.current.capture();
-            const res = await Share.shareSingle({
-              title: 'ReactNative Share MVP',
-              subject: 'ReactNative Share MVP',
-              message: 'Something useful I would say!',
-              email: 'jstrfaheem065@gmail.com',
-              social: Share.Social.EMAIL,
-              failOnCancel: false,
-              urls: [uri],
-            });
-            console.log({res});
+            try {
+              const uri = await captureRef.current.capture();
+              const res = await Share.shareSingle({
+                title: 'ReactNative Share MVP',
+                subject: 'ReactNative Share MVP',
+                message: 'Something useful I would say!',
+                email: 'jstrfaheem065@gmail.com',
+                social: Share.Social.EMAIL,
+                failOnCancel: false,
+                urls: [uri],
+              });
+              console.log({res});
+            } catch (e) {
+              console.log({e});
+            }
           }}
           title="EMAIL SHARE"></Button>
         <View style={{marginTop: 20}}></View>
         <Button
           onPress={async () => {
-            const uri = await captureRef.current.capture();
-            const res = await Share.shareSingle({
-              title: 'ReactNative Instagram Share MVP',
-              social: Share.Social.INSTAGRAM,
-              url: uri,
-              failOnCancel: false,
-            });
-            console.log({res});
+            try {
+              const uri = await captureRef.current.capture();
+              // const res = await Share.shareSingle({
+              //   // title: 'ReactNative Instagram Share MVP',
+              //   // social: Share.Social.INSTAGRAM,
+              //   // url: uri,
+              //   // forceDialog: true,
+              //   // title: 'Share via',
+              //   // method: Share.InstagramStories.SHARE_BACKGROUND_IMAGE,
+              //   // backgroundImage: uri,
+              //   // social: Share.Social.INSTAGRAM_STORIES,
+              //   // url: 'http://www.google.com',
+              // });
+              await Share.shareSingle({
+                social: Share.Social.INSTAGRAM,
+                url: uri,
+                type: 'image/*',
+              });
+              console.log({res});
+            } catch (e) {
+              console.log({e});
+            }
           }}
           title="INSTAGRAM SHARE"></Button>
       </ViewShot>
