a885243280dacf2479fde621c4ab478d1f4829c4
FreeDoM <jstrfaheem065@gmail.com>
2022-06-22 16:07:08 +0530

    Package name changed,

---
+++ grnfit/App.js
@@ -0,0 +1,32 @@
+import React, {useEffect} from 'react';
+import {View} from 'react-native';
+import GoogleFit, {BucketUnit, Scopes} from 'react-native-google-fit';
+
+const App = () => {
+  useEffect(() => {
+    const options = {
+      scopes: [Scopes.FITNESS_ACTIVITY_READ, Scopes.FITNESS_ACTIVITY_WRITE],
+    };
+    GoogleFit.authorize(options)
+      .then(res => {
+        console.log({res});
+        GoogleFit.getDailyStepCountSamples({
+          bucketUnit: BucketUnit.HOUR,
+        })
+          .then(results => {
+            console.log({
+              results,
+            });
+          })
+          .catch(error => {
+            console.log({error});
+          });
+      })
+      .catch(err => {
+        console.log({err});
+      });
+  }, []);
+  return <View></View>;
+};
+
+export default App;
