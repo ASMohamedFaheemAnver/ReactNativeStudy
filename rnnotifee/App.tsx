b319599ae0301b32ad965240585f0ab57e0d916e
FreeDoM <jstrfaheem065@gmail.com>
2023-03-21 08:53:35 +0530

    Request permission and getting token,

--- rnnotifee/App.tsx
+++ rnnotifee/App.tsx
@@ -1,7 +1,7 @@
 import notifee from '@notifee/react-native';
+import messaging from '@react-native-firebase/messaging';
 import React from 'react';
 import {Button, SafeAreaView, ScrollView, View} from 'react-native';
-
 function App(): JSX.Element {
   async function onDisplayLocalNotification() {
     await notifee.requestPermission();
@@ -25,11 +25,31 @@ function App(): JSX.Element {
     });
   }
 
+  async function onAllowRemoteNotification() {
+    // Not for push notification but for receiving message from external providers
+    // This will ask permission from
+    const authStatus = await messaging().requestPermission();
+    const enabled =
+      authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+      authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+    console.log({authStatus, enabled});
+    if (enabled) {
+      // Before getting token we need to register
+      // On ios if we need to receive message
+      if (!messaging().isDeviceRegisteredForRemoteMessages) {
+        await messaging().registerDeviceForRemoteMessages();
+      }
+      const token = await messaging().getToken();
+      console.log({token});
+    }
+  }
+
   return (
     <SafeAreaView>
       <ScrollView>
         <View>
           <Button onPress={onDisplayLocalNotification} title="LOCAL PUSH" />
+          <Button onPress={onAllowRemoteNotification} title="REMOTE PUSH" />
         </View>
       </ScrollView>
     </SafeAreaView>
