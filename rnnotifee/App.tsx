0d8175e36907472debe8d2ce48e76a3988480094
FreeDoM <jstrfaheem065@gmail.com>
2023-03-21 08:31:31 +0530

    Local push notification basic,

--- rnnotifee/App.tsx
+++ rnnotifee/App.tsx
@@ -1,11 +1,36 @@
+import notifee from '@notifee/react-native';
 import React from 'react';
-import {SafeAreaView, ScrollView, View} from 'react-native';
+import {Button, SafeAreaView, ScrollView, View} from 'react-native';
 
 function App(): JSX.Element {
+  async function onDisplayLocalNotification() {
+    await notifee.requestPermission();
+    // This will create notification channel
+    const channelId = await notifee.createChannel({
+      id: 'default',
+      // Any name
+      name: 'DEFAULT CHANNEL',
+    });
+    await notifee.displayNotification({
+      title: 'TITLE',
+      body: 'BODY OF THE NOTIFICATION',
+      android: {
+        channelId,
+        // smallIcon: 'ic_launcher',
+        pressAction: {
+          // Id should be something not random
+          id: 'default',
+        },
+      },
+    });
+  }
+
   return (
     <SafeAreaView>
       <ScrollView>
-        <View></View>
+        <View>
+          <Button onPress={onDisplayLocalNotification} title="LOCAL PUSH" />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
