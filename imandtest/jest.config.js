c6a29230b319f376834238041a5ad1abcf9a9f0b
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-05-08 19:56:29 +0530

    ESM Issue fixed,

--- imandtest/jest.config.js
+++ imandtest/jest.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   preset: 'react-native',
+  transformIgnorePatterns: [
+    // 'node_modules/(?!@react-native|module1|module2)',
+    'node_modules/(?!@react-native|react-native|react-redux)',
+  ],
 };
