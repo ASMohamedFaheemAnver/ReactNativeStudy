17b03b3ea50b056dfdcd2a5c5b5808b9a33d883a
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-05-08 18:50:49 +0530

    Redux configured,

--- imandtest/App.tsx
+++ imandtest/App.tsx
@@ -1,13 +1,17 @@
 import CustomText from '@components/Text';
 import Home from '@screens/Home';
+import {store} from '@src/redux/store';
 import {View} from 'react-native';
+import {Provider} from 'react-redux';
 
 function App(): React.JSX.Element {
   return (
-    <View>
-      <Home></Home>
-      <CustomText />
-    </View>
+    <Provider store={store}>
+      <View>
+        <Home></Home>
+        <CustomText />
+      </View>
+    </Provider>
   );
 }
 
