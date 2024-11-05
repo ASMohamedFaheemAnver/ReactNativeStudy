459cb089b9282ce57eaa2e520a1f19c2c49f58c5
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-11-05 12:33:17 +0530

    IOS setup with pre action and workaround script,

--- multienv/App.tsx
+++ multienv/App.tsx
@@ -1,16 +1,15 @@
-import {Text, View} from 'react-native';
+import {SafeAreaView, Text, View} from 'react-native';
 import Config from 'react-native-config';
 import DeviceInfo from 'react-native-device-info';
 
 const App = () => {
   console.log();
   return (
-    <View>
+    <SafeAreaView>
       <Text>{`Current env : ${Config.NODE_ENV}`}</Text>
       <Text>{`Package name : ${DeviceInfo.getBundleId()}`}</Text>
       <Text>{`App name : ${DeviceInfo.getApplicationName()}`}</Text>
-      <Text>{`Permissions : ${DeviceInfo.getApplicationName()}`}</Text>
-    </View>
+    </SafeAreaView>
   );
 };
 
