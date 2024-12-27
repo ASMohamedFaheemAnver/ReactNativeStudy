3b17ee52f8c5db39236d388fbe60bc628307043a
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-12-27 22:26:00 +0530

    Basic ios module starter

---
+++ modules/basicIos/App.tsx
@@ -0,0 +1,15 @@
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
