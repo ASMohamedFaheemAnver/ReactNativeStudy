2e8b47b60427be484691f5c7073701586a2826f1
ASMohamedFaheemAnver <jstrfaheem065@gmail.com>
2024-12-28 12:32:52 +0530

    Android module starter

---
+++ modules/basicAndroid/App.tsx
@@ -0,0 +1,16 @@
+import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import React from 'react';
+
+type Props = {};
+
+const App = (props: Props) => {
+  return (
+    <SafeAreaView>
+      <Text>App</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({});
